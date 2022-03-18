89ad3fa3499d7db38fc2d1ef224c4f8865787c7d
francesco-plc <s281705@studenti.polito.it>
2022-03-18 16:29:37 +0100

    Created server module

---
+++ server/src/test/kotlin/it/polito/wa2/group09/lab2/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package it.polito.wa2.group09.lab2.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
