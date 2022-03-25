f1634ef589f21c1c41a15efccf23137d08757211
francesco-plc <s281705@studenti.polito.it>
2022-03-25 18:09:10 +0100

    Fixed USL

--- server/src/main/kotlin/it/polito/wa2/group09/lab2/server/USL.kt
+++ server/src/main/kotlin/it/polito/wa2/group09/lab2/server/USL.kt
@@ -7,6 +7,7 @@ import org.apache.commons.csv.CSVPrinter
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
+import java.text.DecimalFormat
 
 
 class USL {
@@ -14,7 +15,7 @@ class USL {
         @JvmStatic public fun main(args : Array<String>){
             fun buildModel() {
                 val writer = Files.newBufferedWriter(Paths.get("server/src/main/resources/usl.csv"))
-                val csvPrinter = CSVPrinter(writer, CSVFormat.DEFAULT.withRecordSeparator("\n").withDelimiter(';')
+                val csvPrinter = CSVPrinter(writer, CSVFormat.EXCEL.withDelimiter(';')
                     .withHeader("Concurrency Level","Req/Sec"))
                 val points =
                     arrayOf(doubleArrayOf(1.0, 156.0), doubleArrayOf(2.0, 313.0), doubleArrayOf(4.0, 510.0), doubleArrayOf(8.0, 831.0), doubleArrayOf(16.0, 1115.0), doubleArrayOf(32.0, 1100.0))
@@ -25,12 +26,13 @@ class USL {
                         Measurement.ofConcurrency().andThroughput(point)
                     }
                     .collect(Model.toModel())
-                var i = 50
+                var i = 10
 
                 while (i <= 10000) {
-                    System.out.printf("At %d workers, expect %f req/sec\n", i, model.throughputAtConcurrency(i.toDouble()))
-                    csvPrinter.printRecord(i,String.format("%.2f",model.throughputAtConcurrency(i.toDouble())).replace(",",".").toFloat())
-                    i += 50
+                    val res = model.throughputAtConcurrency(i.toDouble())
+                    System.out.printf("At %d workers, expect %f req/sec\n", i, res)
+                    csvPrinter.printRecord(i, DecimalFormat("#.##").format(res))
+                    i += 10
                 }
                 csvPrinter.flush()
                 csvPrinter.close()
